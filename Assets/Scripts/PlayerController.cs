338c0148a6894759918d8ec325c72b841b54b5a0
Otto <ottocolla@gmail.com>
2021-01-29 19:03:12 -0300

    Corrected Movement
    *PlayerMovement
    *PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     PlayerMovement _playerMovement;
+
+    int _horizontalMovement;
+    int _verticalMovement;
+
     void Start()
     {
         _playerMovement = GetComponent<PlayerMovement>();
@@ -17,10 +21,16 @@ public class PlayerController : MonoBehaviour
 
     void Movement()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.A)) _horizontalMovement = -1;  
+        if (Input.GetKeyDown(KeyCode.D)) _horizontalMovement = 1;  
+        if (Input.GetKeyDown(KeyCode.S)) _verticalMovement = -1;  
+        if (Input.GetKeyDown(KeyCode.W)) _verticalMovement = 1;
 
-        _playerMovement.Move(new Vector3(horizontal,vertical));
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) _horizontalMovement = 0;
+        if (!Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W)) _verticalMovement = 0;
+
+
+        _playerMovement.Move(new Vector3(_horizontalMovement, _verticalMovement));
 
     }
 }
